4bcb8658f5a4ef8fd31a07a503bce3effe9eda3d
Ning Xia <nxiamail@gmail.com>
2015-06-12 22:39:05 -0700

    finish writing models' get and post

--- server-pure-sql/models/index.js
+++ server-pure-sql/models/index.js
@@ -4,8 +4,7 @@ module.exports = {
   messages: {
     // a function which produces all the messages (SELECT)
     get: function (callback) {
-      // using a left outer join since the user posting
-      //   the message may not yet exist
+      // using a left outer join since the user of a message may not yet exist
       var queryString = 'select messages.id, messages.text, messages.roomname, users.username \
                          from messages left outer join users on (messages.userid = users.id) \
                          order by messages.id desc';  
@@ -13,7 +12,7 @@ module.exports = {
         callback(err, results);
       });                         
     }, 
-    // insert a message into the database (INSERT)
+    // insert a message into the messages table (INSERT)
     post: function (params, callback) {
       var queryString = 'insert into messages(text, userid, roomname) \
                          value (?, select id from users where username = ? limit 1), ?)';
@@ -24,9 +23,21 @@ module.exports = {
   },
 
   users: {
-    // Ditto as above.
-    get: function () {},
-    post: function () {}
+    // a function which produces all the users (SELECT)
+    get: function (callback) {
+      // all users. each unique user is a unique row
+      var queryString = 'select * from users';
+      db.query(queryString, function(err, results){
+        callback(err, results);
+      })
+    },
+    // add  a new user into the users table (INSERT)
+    post: function (params, callback) {
+      var queryString = 'insert into users(username) values (?)';
+      db.query(queryString, params, function(err, results){
+        callback(err, results);
+      });
+    }
   }
 };
 
