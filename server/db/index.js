62fd43277d038497ee57e6ac14cdc4ae5c340aa0
Ning Xia <nxiamail@gmail.com>
2015-06-12 23:42:57 -0700

    Sequelize ORM instance and export in db/index.js

--- server/db/index.js
+++ server/db/index.js
@@ -1,1 +1,23 @@
-var Sequelize 
+var Sequelize = require('sequelize');
+var orm = new Sequelize('chat', 'root', '');
+
+// define tables (which are models)
+var Message = orm.define('Message', {
+  roomname: Sequelize.STRING,
+  text: Sequelize.STRING
+});
+
+var User = orm.define('User', {
+  username: Sequelize.STRING
+});
+
+// define relational structure 
+User.hasMany(Message);    // one to many (one user has many messages)
+Message.belongsTo(User);  // one to one (one message has one user)
+
+// sync these models (if not created, create them already)
+User.sync();
+Message.sync();
+
+exports.User = User;
+exports.Message = Message;
